6289609e64a599031659b6db506e0268f1a85c2a
Thafheem <thafu.safarulla@gmail.com>
2024-08-13 13:59:07 +0400

    update

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function showSlides() {
     
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 400); // Change image every 3 seconds
+    setTimeout(showSlides, 700); // Change image every 3 seconds
 }
 
 function currentSlide(n) {
